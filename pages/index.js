d8b7e7a2c34bf6e5a7464a16ec7d8d8d743c6ee3
devemacao <113943829+devemacao@users.noreply.github.com>
2025-02-04 23:28:49 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Teste</h1>;
+    return (
+    <div> 
+        <h1>Oi preguicinha, te amo de montão!</h1>
+        <p>Meu amor por você é do tamanho do universo &#128584;&#128525;&#129505;.</p>        
+    </div>
+    );
 }
 
 export default Home;
