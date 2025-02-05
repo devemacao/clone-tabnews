ee22a4443ef053a9ede9fa46c3dfb7aaa8397c3c
devemacao <113943829+devemacao@users.noreply.github.com>
2025-02-05 22:18:03 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-function Home(){
-    return (
-    <div> 
-        <h1>Oi preguicinha, te amo de montão!</h1>
-        <p>Meu amor por você é do tamanho do universo &#128584;&#128525;&#129505;.</p>        
+function Home() {
+  return (
+    <div>
+      <h1>Oi preguicinha, te amo de montão!</h1>
+      <p>
+        Meu amor por você é do tamanho do universo &#128584;&#128525;&#129505;.
+      </p>
     </div>
-    );
+  );
 }
 
 export default Home;
