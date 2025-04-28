83265a86cdd85fbdb8ce8170c3532515ad58b178
devemacao <113943829+devemacao@users.noreply.github.com>
2025-04-28 03:11:04 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,25 +6,33 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const firstPostResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-  expect(firstPostResponse.status).toBe(201);
-  const firstPostResponseBody = await firstPostResponse.json();
-  expect(Array.isArray(firstPostResponseBody)).toBe(true);
-  expect(firstPostResponseBody.length).toBeGreaterThan(0);
-  const secondPostResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
-  expect(secondPostResponse.status).toBe(200);
-  const secondPostResponseBody = await secondPostResponse.json();
-  expect(Array.isArray(secondPostResponseBody)).toBe(true);
-  expect(secondPostResponseBody.length).toBe(0);
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const firstPostResponse = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(firstPostResponse.status).toBe(201);
+        const firstPostResponseBody = await firstPostResponse.json();
+        expect(Array.isArray(firstPostResponseBody)).toBe(true);
+        expect(firstPostResponseBody.length).toBeGreaterThan(0);
+      });
+      test("For the second time", async () => {
+        const secondPostResponse = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(secondPostResponse.status).toBe(200);
+        const secondPostResponseBody = await secondPostResponse.json();
+        expect(Array.isArray(secondPostResponseBody)).toBe(true);
+        expect(secondPostResponseBody.length).toBe(0);
+      });
+    });
+  });
 });
